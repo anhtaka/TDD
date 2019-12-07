2596b9288949537634733b88be9aeab3a2d02a66
ponta_gogo677 <ponta_gogo677@yahoo.co.jp>
2019-12-07 13:05:12 +0900

    fix

---
+++ TDDTest.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TDDTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var val = TDD.UI.Caluculation.Sum(2, 5);
+            Assert.AreEqual(7, val);
+        }
+        [TestMethod]
+        public void 平均値を取得できる()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var result = TDD.UI.Caluculation.Ave(list);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
